06d6dcf48fdd45d53fdda56b1f1b848d28059de3
Wesley White <wesleyrwhite86@gmail.com>
2020-04-06 12:58:25 -1000

    Implemented tableViews rows and sections.'

--- iTunesSearch/iTunesSearch/Storyboards/SearchResultsTableViewController.swift
+++ iTunesSearch/iTunesSearch/Storyboards/SearchResultsTableViewController.swift
@@ -8,15 +8,16 @@
 
 import UIKit
 
-class SearchResultsTableViewController: UITableViewController {
+class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
     
     @IBOutlet weak var searchField: UISearchBar!
     
-    
+    let searchController = SearchResultsController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchField.delegate  = self 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -34,18 +35,23 @@ class SearchResultsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return searchController.fromITunesResult.count
     }
 
-    /*
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let singleResult = searchController.fromITunesResult[indexPath.row]
+        
+        cell.textLabel?.text = singleResult.title
+        cell.detailTextLabel?.text = singleResult.creator
+     
+   
+    
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
